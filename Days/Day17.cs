a061aa67957262ce1d148eae2ecbfe10a626aeac
Rafael Zuolo Coppini Lima <rafaelzcl@gmail.com>
2024-12-17 23:01:35 -0300

    Finished Day17 part 1

--- Days/Day17.cs
+++ Days/Day17.cs
@@ -4,7 +4,102 @@ public class Day17 : IDay
 {
     public string SolvePart1(string input)
     {
-        return "not solved";
+        var computer = input.Split(Environment.NewLine + Environment.NewLine, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        var registers = computer[0].Split(Environment.NewLine, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+            .Select(l => long.Parse(l.Split(": ")[1]))
+            .ToArray();
+
+        var regA = registers[0];
+        var regB = registers[1];
+        var regC = registers[2];
+
+        var program = computer[1]
+            .Split(": ")[1]
+            .Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+        var comp = new Computer(program, regA, regB, regC)!;
+        while (comp.CanCompute)
+        {
+            comp.Compute();
+        }
+
+        return comp.ToString();
+    }
+
+    class Computer(int[] program, long regA, long regB, long regC)
+    {
+        public long RegA { get; private set; } = regA;
+        public long RegB { get; private set; } = regB;
+        public long RegC { get; private set; } = regC;
+        public int InstructionPointer { get; private set; } = 0;
+        public int[] Program { get; } = program;
+        public List<int> Output { get; } = [];
+
+        public bool CanCompute => InstructionPointer < Program.Length;
+        public override string ToString() => string.Join(',', Output);
+
+        public void Compute()
+        {
+            switch (Program[InstructionPointer])
+            {
+                case 0:
+                    Adv();
+                    break;
+                case 1:
+                    Bxl();
+                    break;
+                case 2:
+                    Bst();
+                    break;
+                case 3:
+                    Jnz();
+                    InstructionPointer -= 2;
+                    break;
+                case 4:
+                    Bxc();
+                    break;
+                case 5:
+                    Out();
+                    break;
+                case 6:
+                    Bdv();
+                    break;
+                case 7:
+                    Cdv();
+                    break;
+            }
+
+            InstructionPointer += 2;
+        }
+
+        private void Adv() => RegA = RegA >> (int)Combo;
+        private void Bdv() => RegB = RegA >> (int)Combo;
+        private void Cdv() => RegC = RegA >> (int)Combo;
+        private void Out() => Output.Add((int)(Combo % 8));
+        private void Bxc() => RegB = RegB ^ RegC;
+        private void Bst() => RegB = Combo % 8;
+        private void Bxl() => RegB = RegB ^ Literal;
+        private void Jnz()
+        {
+            if (RegA == 0)
+            {
+                InstructionPointer += 2;
+                return;
+            };
+
+            InstructionPointer = (int)Literal;
+        }
+
+        private long Literal => Program[InstructionPointer + 1];
+        private long Combo => Program[InstructionPointer + 1] switch
+        {
+            < 4 => Literal,
+            4 => RegA,
+            5 => RegB,
+            6 => RegC,
+            _ => throw new NotImplementedException(),
+        };
     }
 
     public string SolvePart2(string input)
